a85fb3cfb8c3e2bb25786a70bf053d755b1dc0a1
Bincy Narath <bincybaburajnarath@gmail.com>
2020-09-23 00:10:04 -0400

    made public

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // Mapbox API key
-const API_KEY = "pk.eyJ1IjoiYm5hcmF0aCIsImEiOiJja2ZldXlyYjAwNXF3MnNvMDNxMWxoeTE5In0.ZtmbQ79yFSOniAngz5whaA";
+const API_KEY = "pk.eyJ1IjoiYm5hcmF0aCIsImEiOiJja2V6d29janEwY3o5MnZwNHdjOGp2dHN4In0.0V42dsLyAyQgqgzKNJuWLg";
