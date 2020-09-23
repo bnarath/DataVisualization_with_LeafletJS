0ffb8837518bb6c3582c505f4f4431fb7c000d39
Bincy Narath <bincybaburajnarath@gmail.com>
2020-09-22 23:07:33 -0400

    Removed and changed key

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // Mapbox API key
-const API_KEY = "pk.eyJ1IjoiYm5hcmF0aCIsImEiOiJja2V6d2ZmdWswY3Y0MnJwNzJ5eG85OWdjIn0.CR2szKDP_ROD9fNYyyWCYA";
+const API_KEY = "pk.eyJ1IjoiYm5hcmF0aCIsImEiOiJja2Zlcm9qMzMwN2tuMnNwaTE5bjBicHVhIn0.-ToOC7af1mgrMEJ989kvZg";
